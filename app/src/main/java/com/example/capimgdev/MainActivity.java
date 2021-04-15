cfa19b3878d5354450f614b2b03ee8e1572fb653
pablo <julian.blanco1234@gmail.com>
2021-04-15 16:30:26 -0300

    Se Logro sacar una foto mediante Intent y ademas guardarla en el dispositivo. FALTA AGREGAR EL TIMESTAMP

--- app/src/main/java/com/example/capimgdev/MainActivity.java
+++ app/src/main/java/com/example/capimgdev/MainActivity.java
@@ -3,22 +3,31 @@ package com.example.capimgdev;
 import androidx.annotation.Nullable;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.FileProvider;
 
 import android.Manifest;
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+
+import java.io.File;
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
     static final int REQUES_IMAGE_CAPTURE = 1;
     Button BtnCamera;
     ImageView imageView;
+    String currentPhotoPath;
 
     @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
@@ -42,17 +51,42 @@ public class MainActivity extends AppCompatActivity {
     private void dispatchTakePictureIntent(){
         Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
         if(takePictureIntent.resolveActivity(getPackageManager()) != null){
-            startActivityForResult(takePictureIntent, REQUES_IMAGE_CAPTURE);
+
+            File imagenArchivo = null;
+
+            try{
+                imagenArchivo = crearImagen();
+            } catch (IOException ex){
+                Log.e("Error",ex.toString());
+            }
+            if(imagenArchivo != null){
+                Uri fotoUri = FileProvider.getUriForFile(this, "com.example.capimgdev.fileprovider", imagenArchivo);
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, fotoUri);
+                startActivityForResult(takePictureIntent, REQUES_IMAGE_CAPTURE);
+            }
         }
     }
 
 
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         if(requestCode == REQUES_IMAGE_CAPTURE && resultCode == RESULT_OK){
-            Bundle extras = data.getExtras();
-            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            //Modifico el muestreo para que lo haga con el archivo guardado.
+            //Bundle extras = data.getExtras();
+            //Bitmap imageBitmap = (Bitmap) extras.get("data");
+            //imageView.setImageBitmap(imageBitmap);
+
+            Bitmap imageBitmap = BitmapFactory.decodeFile(currentPhotoPath);
             imageView.setImageBitmap(imageBitmap);
 
         }
+    }
+
+    private File crearImagen() throws IOException {
+        String nombreImagen = "foto_";
+        File directorio = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File imagen = File.createTempFile(nombreImagen , ".jpg",directorio);
+
+        currentPhotoPath = imagen.getAbsolutePath();
+        return imagen;
     }
 }
