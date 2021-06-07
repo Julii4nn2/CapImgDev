6b07a44277a379417e972369e0b2e5c29001929d
Julii4nn2 <56120552+Julii4nn2@users.noreply.github.com>
2021-06-07 01:19:22 -0300

    Construccion de vista previa y modificacion de layout para la nueva camara

--- app/src/main/java/com/example/capimgdev/MainActivity.java
+++ app/src/main/java/com/example/capimgdev/MainActivity.java
@@ -15,6 +15,7 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.hardware.Camera;
 import android.net.Uri;
+import android.nfc.Tag;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
@@ -26,6 +27,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import java.io.File;
 import java.io.IOException;
@@ -38,6 +40,8 @@ public class MainActivity extends AppCompatActivity {
     Button BtnCamera;
     ImageView imageView;
     String currentPhotoPath;
+    Camera mCamera = null;
+    private SurfaceHolder mHolder;
 
     @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
@@ -48,20 +52,19 @@ public class MainActivity extends AppCompatActivity {
         // solicitar permisos para utilizar la camara
         requestPermissions(new String[]{ Manifest.permission.CAMERA }, 1);
         requestPermissions(new String[]{ Manifest.permission.WRITE_EXTERNAL_STORAGE},1);
-        //Debo instanciar el objeto camara
-        public static Camera getCameraInstance(){
-            Camera camera = null;
-            try {
-                camera = Camera.open(); //Crea un instanciado para la camara
-            }
-            catch (Exception e){
-                Log.e("La camara no esta disponible",ex.toString());
-            }
+
+        //Instancio la camara
+        try{
+            mCamera = Camera.open();
+        }catch (Exception e){
+            // La camara no esta disponible
+            Toast.makeText(this,"La camara no esta disponible", Toast.LENGTH_LONG).show();
+
         }
 
 
-        BtnCamera = findViewById(R.id.BtnCamara);
-        imageView = findViewById(R.id.imageView);
+        BtnCamera = findViewById(R.id.button_capture);
+        //mHolder = findViewById(R.id.camera_preview);
 
         BtnCamera.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -74,7 +77,70 @@ public class MainActivity extends AppCompatActivity {
     // Se comienza el desarrollo pra el objeto camara y manejarlo de manera manual
 
 
+    public class CameraPreview extends SurfaceView implements SurfaceHolder.Callback{
 
+
+
+        public CameraPreview(Context context, Camera camera){
+            super(context);
+            mCamera = camera;
+            mHolder = getHolder();
+            mHolder.addCallback(this);
+
+            mHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+
+
+        }
+        @Override
+        public void surfaceCreated(@NonNull SurfaceHolder holder) {
+            // The Surface has been created, now tell the camera where to draw the preview.
+            try {
+                mCamera.setPreviewDisplay(holder);
+                mCamera.startPreview();
+            } catch (IOException e) {
+              //  Toast.makeText(this,"Error al configurar la vista previa",Toast.LENGTH_LONG).show();
+                //Log.d(TAG, "Error setting camera preview: " + e.getMessage()
+            }
+
+        }
+
+        @Override
+        public void surfaceChanged(@NonNull SurfaceHolder holder, int format, int width, int height) {
+            // If your preview can change or rotate, take care of those events here.
+            // Make sure to stop the preview before resizing or reformatting it.
+
+            if (mHolder.getSurface() == null){
+                // preview surface does not exist
+                return;
+            }
+
+            // stop preview before making changes
+            try {
+                mCamera.stopPreview();
+            } catch (Exception e){
+                // ignore: tried to stop a non-existent preview
+            }
+
+            // set preview size and make any resize, rotate or
+            // reformatting changes here
+
+            // start preview with new settings
+            try {
+                mCamera.setPreviewDisplay(mHolder);
+                mCamera.startPreview();
+
+            } catch (Exception e){
+                //Toast.makeText(this,"Error al iniciar la vista previa",Toast.LENGTH_LONG).show();
+                //Log.d(Tag, "Error starting camera preview: " + e.getMessage());
+            }
+        }
+
+        @Override
+        public void surfaceDestroyed(@NonNull SurfaceHolder holder) {
+
+            mCamera.release();
+        }
+    }
     // FIN DEL DESARROLLO DE LA CAMARA MANUAL
 
 
